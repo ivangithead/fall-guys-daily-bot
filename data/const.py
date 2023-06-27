77f17b783859d5f282f2f7f1e3d00f2a6807624c
ivan <59779051+ivangithead@users.noreply.github.com>
2023-06-27 16:35:36 +0300

    Update const.py

--- data/const.py
+++ data/const.py
@@ -7,18 +7,3 @@ API from https://fallguys-db.pages.dev/store
 """
 
 URL = "https://api2.fallguysdb.info/api/symphony-store"
-
-"""
-Image assets
-"""
-
-
-subtitles = {
-    "cosmetics_nameplates": "Табличка для имени",
-    "costumes_faceplates":  "Лицо",
-    "costumes_patterns":    "Название на табличке",
-    "cosmetics_emotes":     "Эмоция",
-    "costumes_lower":       "Нижняя часть костюма",
-    "costumes_upper":       "Верхняя часть костюма",
-    # Need: Win scene
-}
